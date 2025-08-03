f748d2b8ce81eda5dc123feff5d39abd1fd5bc10
Mayra30345 <palmayra966@gmail.com>
2025-08-03 10:09:48 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,7 @@
+age=int(input("enter your age: "))
+if 10<=age <=20:
+    print("Your age is between 10 to 20 years old ")
+
+else: 
+        print("Your age is not between 10 to 20 years old")
+    
